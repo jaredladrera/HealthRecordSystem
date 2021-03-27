2f706a215be773f84f7ef4715d2dad4b8b52ef07
jaredladrera21@gmail.com <jaredladrera21@gmail.com>
2021-03-27 21:24:06 +0800

    updating database

--- database/health_records.sql
+++ database/health_records.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 09, 2021 at 03:35 PM
--- Server version: 10.1.31-MariaDB
--- PHP Version: 7.2.3
+-- Generation Time: Mar 27, 2021 at 02:22 PM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.2.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -32,11 +31,12 @@ CREATE TABLE `accountinformation` (
   `id` int(11) NOT NULL,
   `name` varchar(50) NOT NULL,
   `lastname` varchar(100) NOT NULL,
+  `address` varchar(100) NOT NULL,
   `middle_name` varchar(100) NOT NULL,
   `age` varchar(10) NOT NULL,
   `gender` varchar(10) NOT NULL,
   `username` varchar(100) NOT NULL,
-  `password` varchar(100) NOT NULL,
+  `account_password` varchar(100) NOT NULL,
   `contact_number` varchar(100) NOT NULL,
   `id_number` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
@@ -47,17 +47,48 @@ CREATE TABLE `accountinformation` (
 -- Dumping data for table `accountinformation`
 --
 
-INSERT INTO `accountinformation` (`id`, `name`, `lastname`, `middle_name`, `age`, `gender`, `username`, `password`, `contact_number`, `id_number`, `email`, `account_status`) VALUES
-(1, 'lance', 'cabiscuelas', 'ladrera', '15', 'male', 'lance21', 'lance21', '09284', 's2038589', 'ladrera21@gmail.com', 'user');
+INSERT INTO `accountinformation` (`id`, `name`, `lastname`, `address`, `middle_name`, `age`, `gender`, `username`, `account_password`, `contact_number`, `id_number`, `email`, `account_status`) VALUES
+(1, 'Lance Jared Cabiscuelas', 'cabiscuelas', 'Malvar Batangas', 'ladrera', '15', 'male', 'lance21', 'lance21', '09307980536', 's2038589', 'ladrera21@gmail.com', 'nurse');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patients`
+--
+
+CREATE TABLE `patients` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `lastname` varchar(100) NOT NULL,
+  `id_number` varchar(50) NOT NULL,
+  `issue` varchar(100) NOT NULL,
+  `contact_number` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `gender` varchar(20) NOT NULL,
+  `note` varchar(200) NOT NULL,
+  `age` varchar(20) NOT NULL,
+  `guardian` varchar(100) NOT NULL,
+  `parent_contact` varchar(50) NOT NULL,
+  `issue_status` varchar(20) NOT NULL,
+  `date_issue` varchar(50) NOT NULL,
+  `time_issue` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `patients`
+--
+
+INSERT INTO `patients` (`id`, `name`, `lastname`, `id_number`, `issue`, `contact_number`, `address`, `gender`, `note`, `age`, `guardian`, `parent_contact`, `issue_status`, `date_issue`, `time_issue`) VALUES
+(1, 'Lance Jared Cabiscuelas', 'Cabiscuelas', 'jhgkjk', 'gkgk', '09307980536', 'Malvar Batangas', 'male', 'fxgfff', 'g456', 'Lance Jared Cabiscuelas', '09307980536', 'minor', '2021-03-04', '1:17 PM');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `accountinformation`
+-- Indexes for table `patients`
 --
-ALTER TABLE `accountinformation`
+ALTER TABLE `patients`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -65,9 +96,9 @@ ALTER TABLE `accountinformation`
 --
 
 --
--- AUTO_INCREMENT for table `accountinformation`
+-- AUTO_INCREMENT for table `patients`
 --
-ALTER TABLE `accountinformation`
+ALTER TABLE `patients`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
